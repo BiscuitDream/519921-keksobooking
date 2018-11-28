a3b484d6486ca7da3ef2501ef8ddb8258e969120
Igor Pechenkin <ipechenkin@gmail.com>
2018-11-28 22:12:39 +0300

    Генерирую объекты, пока не полностью

--- js/map.js
+++ js/map.js
@@ -45,21 +45,76 @@ var OFFERS_PHOTOS = [
   'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
 ];
 
+var OFFERS_PRICES = {
+  min: 1000,
+  max: 1000000
+};
+
+var OFFERS_ROOMS = {
+  min: 1,
+  max: 5
+};
+
+var OFFERS_GUESTS = {
+  min: 1,
+  max: 4
+};
+
+var OFFERS_Y = {
+  min: 130,
+  max: 630
+};
+
+var OFFERS_X = {
+  min: 130,
+  max: 630
+};
+
+
 var getRandomIndex = function (array) {
   return Math.floor(Math.random() * array.length);
 };
 
 var getRandomElement = function (array) {
-  return array[getRandomIndex];
-}
+  return array[getRandomIndex(array)];
+};
 
-var getRandomNumber = function (min, max) {
+var getRandomInteger = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
+var shuffleArray = function (array) {
+  return array;
+};
 
+var generateOffersArray = function () {
+  var adverts = [];
 
+  for (var i = 0; i < 8; i++) {
+    adverts[i] = {
+      avatar: 'img/avatars/user0' + getRandomInteger(0, 8) + '.png',
+      title: getRandomElement(OFFERS_TITLES),
+      address: '',
+      price: getRandomInteger(OFFERS_PRICES.min, OFFERS_PRICES.max),
+      type: getRandomElement(OFFERS_TYPES),
+      rooms: getRandomInteger(OFFERS_ROOMS.min, OFFERS_ROOMS.max),
+      guests: getRandomInteger(OFFERS_GUESTS.min, OFFERS_GUESTS.max),
+      checkin: getRandomElement(OFFERS_CHECKIN_TIMES),
+      checkout: getRandomElement(OFFERS_CHECKOUT_TIMES),
+      features: getRandomElement(OFFERS_FEATURES),
+      description: '',
+      photos: OFFERS_PHOTOS,
+      location: {
+        x: getRandomInteger(OFFERS_X.min, OFFERS_X.max),
+        y: getRandomInteger(OFFERS_Y.min, OFFERS_Y.max)
+      }
+    };
+  }
 
+  return adverts;
+};
+
+var ar = generateOffersArray();
 
 
 // document.querySelector('.map').classList.remove('map--faded');
