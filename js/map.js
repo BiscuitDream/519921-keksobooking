0d78707cd8cde39f3c015d1bff9bf2815465c171
Igor Pechenkin <ipechenkin@gmail.com>
2018-12-10 14:26:19 +0300

    Изменение поля кол-ва гостей при переключении поля кол-ва комнат

--- js/map.js
+++ js/map.js
@@ -382,20 +382,24 @@ numberOfRoomsSelect.addEventListener('change', function () {
     capacitySelect.options[1].disabled = true;
     capacitySelect.options[2].disabled = false;
     capacitySelect.options[3].disabled = true;
+    capacitySelect.selectedIndex = 2;
   } else if (numberOfRoomsSelect.value === '2') {
     capacitySelect.options[0].disabled = true;
     capacitySelect.options[1].disabled = false;
     capacitySelect.options[2].disabled = false;
     capacitySelect.options[3].disabled = true;
+    capacitySelect.selectedIndex = 2;
   } else if (numberOfRoomsSelect.value === '3') {
     capacitySelect.options[0].disabled = false;
     capacitySelect.options[1].disabled = false;
     capacitySelect.options[2].disabled = false;
     capacitySelect.options[3].disabled = true;
+    capacitySelect.selectedIndex = 2;
   } else if (numberOfRoomsSelect.value === '100') {
     capacitySelect.options[0].disabled = true;
     capacitySelect.options[1].disabled = true;
     capacitySelect.options[2].disabled = true;
     capacitySelect.options[3].disabled = false;
+    capacitySelect.selectedIndex = 3;
   }
 });
