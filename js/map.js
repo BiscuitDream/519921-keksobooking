f7f0c775c269b3fa287aa7a4750688ac547d11be
Igor Pechenkin <ipechenkin@gmail.com>
2018-12-11 17:47:00 +0300

    Начальный drag-n-drop

--- js/map.js
+++ js/map.js
@@ -418,3 +418,41 @@ var onNumberOfRoomsSelectChange = function () {
 };
 
 numberOfRoomsSelect.addEventListener('change', onNumberOfRoomsSelectChange);
+
+
+// drag-n-drop
+mapPinMain.addEventListener('mousedown', function (evt) {
+  evt.preventDefault();
+
+  var startCoords = {
+    x: evt.clientX,
+    y: evt.clientY
+  };
+
+  var onMouseMove = function (moveEvt) {
+    moveEvt.preventDefault();
+
+    var shift = {
+      x: startCoords.x - moveEvt.clientX,
+      y: startCoords.y - moveEvt.clientY
+    };
+
+    startCoords = {
+      x: moveEvt.clientX,
+      y: moveEvt.clientY
+    };
+
+    mapPinMain.style.left = (mapPinMain.offsetLeft - shift.x) + 'px';
+    mapPinMain.style.top = (mapPinMain.offsetTop - shift.y) + 'px';
+  };
+
+  var onMouseUp = function (upEvt) {
+    upEvt.preventDefault();
+
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+  };
+
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('mouseup', onMouseUp);
+});
