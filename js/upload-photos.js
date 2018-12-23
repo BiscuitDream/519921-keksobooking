a497bc800baae852face77dac16d9917a4f1acec
Igor Pechenkin <ipechenkin@gmail.com>
2018-12-23 04:15:03 +0300

    Навел марафет в модуле загрузки фото

--- js/upload-photos.js
+++ js/upload-photos.js
@@ -2,9 +2,38 @@
 // 6.1. В форме подачи объявления показывается аватарка пользователя и фотографии объявления при изменении значений соответствующих полей.
 (function () {
   var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
+  var DEFAULT_AVATAR = 'img/muffin-grey.svg';
 
   var avatarChooser = document.querySelector('.ad-form__field input[type=file]'); // элемент выбора аватарки
   var avatarPreview = document.querySelector('.ad-form-header__preview img'); // элемент предпросмотра аватарки
+  var photoChooser = document.querySelector('.ad-form__upload input[type=file]');
+  var photoPreview = document.querySelector('.ad-form__photo');
+
+  var Image = {
+    WIDTH: '70px',
+    HEIGHT: '70px'
+  };
+
+  var renderPhotos = function (photo) {
+    var reader = new FileReader();
+
+    reader.addEventListener('load', function () {
+      var image = document.createElement('img');
+      image.src = reader.result;
+      image.style.width = Image.WIDTH;
+      image.style.height = Image.HEIGHT;
+      photoPreview.appendChild(image);
+    });
+
+    reader.readAsDataURL(photo);
+  };
+
+  var removeImages = function () {
+    if (avatarPreview.src !== DEFAULT_AVATAR) {
+      avatarPreview.src = DEFAULT_AVATAR;
+    }
+    photoPreview.innerHTML = '';
+  };
 
   avatarChooser.addEventListener('change', function () {
     var file = avatarChooser.files[0];
@@ -25,28 +54,6 @@
     }
   });
 
-  var Image = {
-    WIDTH: '70px',
-    HEIGHT: '70px'
-  };
-
-  var photoChooser = document.querySelector('.ad-form__upload input[type=file]');
-  var photoPreview = document.querySelector('.ad-form__photo');
-
-  var renderPhotos = function (photo) {
-    var reader = new FileReader();
-
-    reader.addEventListener('load', function () {
-      var image = document.createElement('img');
-      image.src = reader.result;
-      image.style.width = Image.WIDTH;
-      image.style.height = Image.HEIGHT;
-      photoPreview.appendChild(image);
-    });
-
-    reader.readAsDataURL(photo);
-  };
-
   photoChooser.addEventListener('change', function () {
     var photos = photoChooser.files;
 
@@ -64,15 +71,6 @@
       }
     });
   });
-
-  var DEFAULT_AVATAR = 'img/muffin-grey.svg';
-
-  var removeImages = function () {
-    if (avatarPreview.src !== DEFAULT_AVATAR) {
-      avatarPreview.src = DEFAULT_AVATAR;
-    }
-    photoPreview.innerHTML = '';
-  };
 
   window.uploadPhotos = {
     removeImages: removeImages
